856131d9ae3bfdcddff0a93ae308cd19579cca70
hintzID <habibbenzed22@gmail.com>
2023-10-20 20:03:18 +0700

    versi1

--- footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    &copy; 2023 Toko Hidayah Mesin Jahit. Hak cipta dilindungi.
-</footer>
--- home.php
+++
@@ -1,4 +0,0 @@
-<div class="hero-1" id="home">
-    <h2 class="selamat-datang"><span id="type-it"></span></h2><br><br><br>
-    <p class="keterangan-selamat-datang">Kami Menyediakan Berbagai Macam Peralatan Konveksi : Sparepart, Benang Kiloan, Minyak Mesin, Dll</p>
-</div>
